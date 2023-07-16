af64eec22982365d9dac6a4c33ca238e83aa4316
Bryan Escobar <bryanesc26@gmail.com>
2023-07-16 01:25:02 -0600

    Add Click Events on Host and Join buttons

--- Plugins/MultiplayerSess/Source/MultiplayerSess/Private/Menu.cpp
+++ Plugins/MultiplayerSess/Source/MultiplayerSess/Private/Menu.cpp
@@ -2,6 +2,7 @@
 
 
 #include "Menu.h"
+#include "Components/Button.h"
 
 void UMenu::MenuSetup()
 {
@@ -24,5 +25,50 @@ void UMenu::MenuSetup()
 			PlayerController->SetInputMode(InputModeData);
 			PlayerController->SetShowMouseCursor(true);
 		}
+	}
+}
+
+bool UMenu::Initialize()
+{
+	if(!Super::Initialize())
+	{
+		return false;
+	}
+	if(HostButton)
+	{
+		HostButton->OnClicked.AddDynamic(this, &ThisClass::HostButtonClicked);
+	}
+
+	if(JoinButton)
+	{
+		JoinButton->OnClicked.AddDynamic(this, &ThisClass::JoinButtonClicked);
+	}
+	
+	return true;
+}
+
+void UMenu::HostButtonClicked()
+{
+	if (GEngine)
+	{
+		GEngine->AddOnScreenDebugMessage(
+			-1,
+			15.f,
+			FColor::Red,
+			FString(TEXT("host"))
+		);
+	}
+}
+
+void UMenu::JoinButtonClicked()
+{
+	if (GEngine)
+	{
+		GEngine->AddOnScreenDebugMessage(
+			-1,
+			15.f,
+			FColor::Red,
+			FString(TEXT("Join"))
+		);
 	}
 }
